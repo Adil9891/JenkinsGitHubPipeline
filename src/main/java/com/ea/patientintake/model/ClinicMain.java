30584e3308ed8df1ac9407dd04c03cb8c84a2661
Adil Sabir <m.sabir.adil@gmail.com>
2023-07-26 14:13:13 +0100

    test 1

--- src/main/java/com/ea/patientintake/model/ClinicMain.java
+++ src/main/java/com/ea/patientintake/model/ClinicMain.java
@@ -10,7 +10,7 @@ import com.ea.patientintake.service.ClinicCalendar;
 import com.ea.patientintake.util.BMICalculator;
 
 public class ClinicMain {
-  // ceci est un commentaire
+  // ceci est un commentaire  2
 
   private static ClinicCalendar calendar;
 
